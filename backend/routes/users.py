d86cd8958cbcd40492f84539ba2c88c6c814e868
Giiid <gideonfummey1@gmail.com>
2024-07-15 14:50:19 +0100

    Fixed minor issues

--- backend/routes/users.py
+++ backend/routes/users.py
@@ -14,9 +14,18 @@ users = APIRouter()
 
 
 
-# Helper functtions
+# Helper functions
 def get_admin(user: AdminIn, db: Session):
     return db.query(Admin).filter(Admin.username == user.username).first()
+
+
+def authenticate_admin(username: str, password: str, db: Session):
+    admin = get_admin(username, db)
+    if not admin:
+        return False
+    if not verify_password(password, admin.password):
+        return False
+    return admin
 
 
 def create_admin(user: AdminIn, db: Session):
@@ -36,36 +45,7 @@ async def create_admin_route(userin: AdminIn, db: Session = Depends(get_db)):
     if db_admin:
         raise HTTPException(status_code=400, detail="Username already exists")
     new_admin = create_admin(userin, db)
-
     return new_admin
-
-
-# get current user
-# async def get_current_admin(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-#     credentials_exception = HTTPException(
-#         status_code= status.HTTP_401_UNAUTHORIZED,
-#         detail= "Could not validate credentials",
-#         headers= {"WWW-Authenticate": "Bearer"}
-#     )
-#     try:
-#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-#         username: str = payload.get("sub")
-#         if username is None:
-#             raise credentials_exception
-#         token_data = TokenData(username=username)
-#     except InvalidTokenError:
-#         raise credentials_exception
-#     current_admin = db.query(Admin).filter(Admin.username == token_data.username).first()
-#     if current_admin is None:
-#         raise credentials_exception
-#     return current_admin
-
-
-# # get current active user
-# async def get_current_active_admin(current_admin: Admin = Depends(get_current_admin)):
-#     if current_admin.disabled:
-#         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Inactive admin")
-#     return current_admin
 
 
 # admin login
@@ -77,29 +57,57 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = 
     if not verify_password(form_data.password, db_admin.password):
         raise HTTPException(status_code=400, detail="Incorrect username or password")
     
-    return {"access_token":"db_admin.username","token":"bearer","message": "Login successfully"}
+    return {"access_token":db_admin.username,"token":"bearer","message": "Login successfully"}
+
+    
+# admin agendas
+@users.get("/v1/{admin_id}/agenda-list", tags=["Admin"])
+async def admin_agenda_list(admin_id: str, db: Session = Depends(get_db)):
+    agendas = db.query(AgendaDb).filter(admin_id == AgendaDb.admin_id).all()
+    if agendas is None:
+        return {"message": "You do not have any agenda"}
+    return agendas
+
+
+# payload to get current admin
+async def get_current_admin(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+    credentials_excepton = HTTPException(
+        status_code = status.HTTP_401_UNAUTHORIZED,
+        detail = "Could not validate credentials",
+        headers = {"WWW-Authenticate": "Bearer"}
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_excepton
+        token_data = TokenData(username = username)
+    except InvalidTokenError:
+        raise credentials_excepton
+    admin = get_admin(user=token_data.username, db=db)
+    if admin is None:
+        raise credentials_excepton
+    return admin
+
+
+# get current active admin
+async def get_current_active_admin(current_admin: Admin = Depends(get_current_admin)):
+    if current_admin.disabled:
+        raise HTTPException(status_code=400, detail="Inactive admin")
+    return current_admin
 
 
 # login for access token
-# @users.post("/v1/login")
-# async def login_access_token(form_data: LoginDb, db: Session = Depends(get_db)):
-#     admin = db.query(Admin).filter(form_data.username == Admin.username).first()
-#     verified_password = verify_password(form_data.password, admin.password)
-#     if not admin or verified_password:
-#         raise HTTPException(
-#             status_code= status.HTTP_401_UNAUTHORIZED,
-#             detail= "Incorrect username or password",
-#             headers={"WWW-Authenticate": "Bearer"}
-#         )
-#     acess_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRES_MINUTES)
-#     access_token = create_access_token(
-#         data={"sub": admin.username}, expires_delta=acess_token_expires
-#         )
-#     return Token(access_token=access_token, token_type="bearer")
-
+@users.post("/v1/admin/login/oauth", tags=["Oauth Login"])
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)) -> Token:
+    admin = authenticate_admin(form_data.username, form_data.password, db)
+    if not admin:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRES_MINUTES)
+    access_token = create_access_token(data={"sub":admin.username}, expires_delta=access_token_expires)
     
-# # admin agendas
-# @users.get("/v1/{admin_id}/agenda-list", tags=["Admin"])
-# async def admin_agenda_list(admin_id: str, db: Session = Depends(get_db)):
-#     agendas = db.query(AgendaDb).filter(admin_id == AgendaDb.admin_id).all()
-#     return agendas
+    return Token(access_token=access_token, token_type="bearer")
