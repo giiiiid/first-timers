97d4d76f256a408197ae0dd89f0a870241e1bdfa
Giiid <gideonfummey1@gmail.com>
2024-07-22 18:08:37 +0100

    Changed location of env

--- backend/config/config.py
+++ backend/config/config.py
@@ -25,11 +25,11 @@ class Settings:
     PROJECT_NAME:str = "First Timers"
     PROJECT_VERSION: str = "1.0.0"
 
-    POSTGRES_USER : str = os.getenv("POSTGRES_USER")
-    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-    POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER","localhost")
-    POSTGRES_PORT : str = os.getenv("POSTGRES_PORT",5432)
-    POSTGRES_DB : str = os.getenv("POSTGRES_DB","tdd")
+    POSTGRES_USER : str = os.environ.get("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+    POSTGRES_SERVER : str = os.environ.get("POSTGRES_SERVER","localhost")
+    POSTGRES_PORT : str = os.environ.get("POSTGRES_PORT",5432)
+    POSTGRES_DB : str = os.environ.get("POSTGRES_DB","tdd")
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
     
 settings = Settings()
@@ -39,8 +39,8 @@ settings = Settings()
 # Oauth setup
 oauth2_scheme = oauth2.OAuth2PasswordBearer(tokenUrl="token")
 
-SECRET_KEY="9592cb2ce44b5d6cf3d8502d4526da89"
-ALGORITHM="HS256"
+SECRET_KEY= os.environ.get("SECRET_KEY")
+ALGORITHM= os.environ.get("ALGORITHM")
 ACCESS_TOKEN_EXPIRES_MINUTES=30
 
 
