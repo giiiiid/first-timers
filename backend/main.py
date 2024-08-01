6be7baa0d1b890b5919acd73bf645ce266e73a9f
Giiid <gideonfummey1@gmail.com>
2024-08-01 23:56:40 +0100

    added cors

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import run, users, forms, agenda
 from models import models
 from db.databaseConnect import engine
@@ -8,6 +9,21 @@ app = FastAPI()
 
 
 models.Base.metadata.create_all(bind=engine)
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://127.0.0.1:8000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 app.include_router(run.run)
