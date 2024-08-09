15e5af7a41bc5a9638db82074043f20f3d9263a9
Giiid <gideonfummey1@gmail.com>
2024-08-09 10:38:12 +0100

    fixedd security issues

--- backend/utils/password_reset.py
+++ backend/utils/password_reset.py
@@ -2,13 +2,13 @@ import os
 from itsdangerous import URLSafeTimedSerializer
 from config.config import SECRET_KEY
 
-SECURITY_PASSWORD_SALT = "gensalt"
+SALT = "gensalt"
 
 
 # generate password reset token function
 def generate_password_reset_token(email):
     serializer = URLSafeTimedSerializer(secret_key=SECRET_KEY)
-    return serializer.dumps(email, salt=SECURITY_PASSWORD_SALT)
+    return serializer.dumps(email, salt=SALT)
 
 
 # verify password reset token function
@@ -17,7 +17,7 @@ def verify_password_reset_token(token, expires=3600):
     try:
         email = serializer.loads(
             token,
-            salt= SECURITY_PASSWORD_SALT,
+            salt= SALT,
             max_age=expires
         )
     except:
